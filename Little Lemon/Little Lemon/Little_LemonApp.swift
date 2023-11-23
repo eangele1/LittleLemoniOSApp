3643729c495b483552b30e005eca5a34d072a057
eangele1 <ezerangeles1@gmail.com>
2023-11-23 14:43:54 -0800

    Initial commit

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Ezer Isai Angeles on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
